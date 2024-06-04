984f845324ff3c63bec6b0e9149186723e59176f
natdm02 <natalidimarino2002@gmail.com>
2024-06-04 15:36:33 +0200

    [add] resolutions

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -1,6 +1,6 @@
+@import "bootstrap/scss/bootstrap";
 import './bootstrap';
-import '~resources/scss/app.scss';
 import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+
+
+console.log('PROVA LOG');
