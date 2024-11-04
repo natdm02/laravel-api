89d9f0c72b92fa93ba9d78de145536c0d6831147
natdm02 <natalidimarino2002@gmail.com>
2024-11-04 15:47:47 +0100

    [add] table structure

--- database/migrations/2024_11_04_143121_create_projects_table.php
+++ database/migrations/2024_11_04_143121_create_projects_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
