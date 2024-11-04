1227a0a4c96702e98228c6594fd10e7c6cbe72c0
natdm02 <natalidimarino2002@gmail.com>
2024-11-04 16:31:43 +0100

    [add]route configuration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\support\Facades\Auth;
+use App\Http\Controllers\Admin\ProjectController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('admin/projects', ProjectController::class);
+});
