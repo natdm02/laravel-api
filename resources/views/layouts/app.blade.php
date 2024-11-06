dc7a489eca5a4ffba4c3d449d6de6321ef7d621c
natdm02 <natalidimarino2002@gmail.com>
2024-11-06 16:03:04 +0100

    [add]custom logout route for user authentication

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,6 +36,12 @@
 
                 </div>
 
+                <form action="{{ route('logout') }}" method="POST">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
+
+
         </nav>
 
     </header>
--- routes/web.php
+++ routes/web.php
@@ -4,14 +4,32 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\support\Facades\Auth;
 use App\Http\Controllers\Admin\ProjectController;
 
+
+
+// rotta di welcome
+
 Route::get('/', function () {
     return view('welcome');
 });
 
+// rotta di autenticazione
+
 Auth::routes();
+
+// rotta della dashboard
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::middleware(['auth'])->group(function () {
-    Route::resource('admin/projects', ProjectController::class);
+// gruppo di rotte per autenticazione
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource( 'projects', ProjectController::class );
 });
+
+
+//rotta del logout
+
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect('/login');
+})->name('logout');
