dded7d50a4413adf494cc7eab594488d71a696c8
natdm02 <natalidimarino2002@gmail.com>
2024-11-10 21:03:34 +0100

    [add]technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,13 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'description', 'type_id'];
+    protected $fillable = ['title', 'description', 'type_id', 'technologies'];
 
     public function type()
 
     {
 
-        return $this->belongsToMany(Type::class);
+        return $this->belongsTo(Type::class);
 
+    }
+
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class, 'project_technology');
     }
 }
