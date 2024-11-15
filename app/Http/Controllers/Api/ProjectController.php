83dd8f7301b1d00bce24ce40747f91afe2138837
natdm02 <natalidimarino2002@gmail.com>
2024-11-15 15:18:28 +0100

    [add] api implementation

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -6,11 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Models\Project;
 
 
+
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('technologies', 'type')->get();
+        $projects = Project::all();
         return response()->json($projects);
     }
 
